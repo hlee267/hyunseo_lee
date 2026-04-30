de100ab132604a00b76f7900737ad0d0565c5377
hlee267 <112427222+hlee267@users.noreply.github.com>
2026-04-30 15:39:51 +0000

    Deploying to gh-pages from @ hlee267/hyunseo_lee@af308c5738a02240cd5ae8e5201ae9282df855e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,28 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/hyunseo_lee/cv/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/hyunseo_lee/people/";
-          },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/hyunseo_lee/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/hyunseo_lee/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
